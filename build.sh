35adefece97912279b1d5284c808026a1df80ae0
Mateus Honorato <mateush.honorato@gmail.com>
2020-10-07 03:34:26 -0300

    use windows version and update to 2.0.9

--- build.sh
+++ build.sh
@@ -17,11 +17,6 @@ if ! 7z --help 2>/dev/null >/dev/null; then
   exit 1
 fi
 
-if ! dmg2img >/dev/null; then
-  error "dmg2img not found"
-  exit 1
-fi
-
 # shellcheck source=config.sh
 source config.sh
 
@@ -33,9 +28,8 @@ if [[ ! -e "deps/electron-$ELECTRON_VERSION.zip" ]]; then
 fi
 
 info "fetching notion $NOTION_VERSION ..."
-if [[ ! -e "deps/notion-$NOTION_VERSION.dmg" ]]; then
-  wget "https://desktop-release.notion-static.com/Notion-$NOTION_VERSION.dmg" -O "deps/notion-$NOTION_VERSION.dmg"
-  dmg2img "deps/notion-$NOTION_VERSION.dmg" "deps/notion-$NOTION_VERSION.img"
+if [[ ! -e "deps/notion-$NOTION_VERSION.exe" ]]; then
+  wget "https://desktop-release.notion-static.com/Notion%20Setup%20$NOTION_VERSION.exe" -O "deps/notion-$NOTION_VERSION.exe"
 fi
 
 info "building electron build ..."
@@ -44,8 +38,9 @@ mkdir -p tmp/build 2>/dev/null || true
 
 set -x
 7z x "deps/electron-$ELECTRON_VERSION.zip" -y -otmp/build >/dev/null
-7z x "deps/notion-$NOTION_VERSION.img" -y  -otmp/notion >/dev/null
-cp -r tmp/notion/Notion**/*.app/Contents/Resources/* tmp/build
+7z x "deps/notion-$NOTION_VERSION.exe" -y  -otmp/notion >/dev/null
+7z x "tmp/notion/\$PLUGINSDIR/app-64.7z" -y  -otmp/notion >/dev/null
+cp -r tmp/notion/resources/* tmp/build
 cp notion tmp/build
 
 # Skip compression
